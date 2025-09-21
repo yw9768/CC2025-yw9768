d8bb4416848bdcd6be0c91d90365ced8c85f9310
yw9768 <yw9768@nyu.edu>
2025-09-21 01:46:58 -0400

    assignment 1

--- assignment1/sketch.js
+++ assignment1/sketch.js
@@ -1,8 +1,180 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+let stick = 2; //variable to store strokeweight size
 
+function setup() {
+  createCanvas(400, 400);// set a 400px by 400ox canvas
 }
 
 function draw() {
+  background("rgba(129, 196, 174, 1)")// set background color
+
+  fill("rgba(191, 242, 185, 1)"); // set the color of complex polygon
+  stroke("rgba(134, 172, 188, 1)"); // set the stroke color of this complex polygons
+  strokeWeight(stick) // set the strokeweight size
+  
+
+  beginShape(); // draw the first complex polygon
+  vertex(57,183);
+  vertex(105,200);
+  vertex(105,270);
+  vertex(153,289);
+  vertex(60,301);
+  endShape(CLOSE); 
+
+  fill("rgba(185, 225, 218, 1)"); // set the color of the first triangle
+  triangle(60,182,105,177,105,200);
+
+  fill("rgba(68, 85, 105, 1)"); // set the color of the second triangle
+  triangle(104,152,105,177,152,170);
+
+  fill("rgba(68, 133, 244, 1)"); // set the color of the third triangle
+  triangle(105,151,153,170,152,146);
+
+  fill("rgba(236, 222, 235, 1)"); // set the color of complex polygon
+  beginShape(); // draw the second complex polygon
+  vertex(105,177);
+  vertex(153,170);
+  vertex(153,219);
+  vertex(105,200);
+  endShape(CLOSE); 
+
+  fill("rgba(235, 217, 180, 1)"); // set the color of complex polygon
+  beginShape(); // draw the third complex polygon
+  vertex(105,200);
+  vertex(153,219);
+  vertex(153,264);
+  vertex(105,270);
+  endShape(CLOSE); 
+
+  fill("rgba(208, 189, 242, 1)"); // set the color of the fourth triangle
+  triangle(107,270,153,264,153,289);
+
+  fill("rgba(208, 189, 242, 1)"); // set the color of the fifth triangle
+  triangle(107,270,153,264,153,289);
+
+  fill("rgba(85, 118, 146, 1)"); // set the color of the sixth triangle
+  triangle(60,301,153,289,153,336);
+
+  fill("rgba(180, 235, 218, 1)"); // set the color of complex polygon
+  beginShape(); // draw the fourth complex polygon
+  vertex(152,146);
+  vertex(203,165);
+  vertex(202,212);
+  vertex(153,219);
+  endShape(CLOSE); 
+
+  fill("rgba(228, 235, 180, 1)"); // set the color of complex polygon
+  beginShape(); // draw the fifth complex polygon
+  vertex(153,219);
+  vertex(202,212);
+  vertex(201,259);
+  vertex(153,264);
+  endShape(CLOSE); 
+
+  fill("rgba(230, 201, 181, 1)"); // set the color of complex polygon
+  beginShape(); // draw the fifth complex polygon
+  vertex(153,264);
+  vertex(201,259);
+  vertex(249,276);
+  vertex(249,324);
+  vertex(153,336);
+  endShape(CLOSE); 
+
+  fill("rgba(230, 201, 181, 1)"); // set the color of complex polygon
+  beginShape(); // draw the fifth complex polygon
+  vertex(153,264);
+  vertex(201,259);
+  vertex(249,276);
+  vertex(249,324);
+  vertex(153,336);
+  endShape(CLOSE); 
+
+  fill("rgba(214, 168, 247, 1)"); // set the color of complex polygon
+  beginShape(); // draw the fifth complex polygon
+  vertex(151,99);
+  vertex(299,152);
+  vertex(203,165);
+  vertex(152,146);
+  endShape(CLOSE); 
+
+  line(252,137,252,158);
+
+  fill("rgba(150, 207, 244, 1)"); // set the color of the seventh triangle
+  triangle(151,99,252,85,252,137);
+
+  fill("rgba(251, 202, 203, 1)"); // set the color of complex polygon
+  beginShape(); // draw the sixth complex polygon
+  vertex(203,165);
+  vertex(252,158);
+  vertex(250,205);
+  vertex(202,212);
+  endShape(CLOSE); 
+
+  fill("rgba(202, 223, 251, 1)"); // set the color of complex polygon
+  beginShape(); // draw the seventh complex polygon
+  vertex(202,212);
+  vertex(250,205);
+  vertex(249,276);
+  vertex(201,259);
+  endShape(CLOSE); 
+
+  fill("rgba(144, 236, 146, 1)"); // set the color of complex polygon
+  beginShape(); // draw the eighth complex polygon
+  vertex(252,158);
+  vertex(299,152);
+  vertex(297,222);
+  vertex(250,205);
+  endShape(CLOSE); 
+
+  fill("rgba(236, 175, 144, 1)"); // set the color of complex polygon
+  beginShape(); // draw the nighth complex polygon
+  vertex(250,205);
+  vertex(297,222);
+  vertex(296,246);
+  vertex(249,252);
+  endShape(CLOSE); 
+
+  fill("rgba(202, 207, 232, 1)"); // set the color of complex polygon
+  beginShape(); // draw the tenth complex polygon
+  vertex(249,252);
+  vertex(296,246);
+  vertex(296,269);
+  vertex(249,276);
+  endShape(CLOSE); 
+
+  line(249,252,296,269);
+
+  fill("rgba(202, 207, 232, 1)"); // set the color of complex polygon
+  beginShape(); // draw the eleventh complex polygon
+  vertex(252,85);
+  vertex(350,123);
+  vertex(345,240);
+  vertex(297,222);
+  vertex(299,152);
+  vertex(252,137);
+  endShape(CLOSE); 
+
+  fill("rgba(85, 118, 146, 1)"); // set the color of the seventh triangle
+  triangle(297,222,345,240,296,246);
+  
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
--- week3/sketch.js
+++ week3/sketch.js
@@ -1,5 +1,5 @@
 // variable declaration:
-//"let" os a keyword that allows you to declare a variable
+//"let" as a keyword that allows you to declare a variable
 // in the below example, a new variable is being created
 //called "circleSize" which is storng a number (125)
 
@@ -7,7 +7,7 @@ let circleSize = 125;// variable to store circle size
 
 function setup() { //runs once at starup
   createCanvas(400, 400); // set a 400px by 400ox canvas
-  circleSizw = width*0.35; // sets circleSize variable in relation to width
+  circleSize = width*0.35; // sets circleSize variable in relation to width
 
 }
 
@@ -19,26 +19,25 @@ function draw() {
   // background ("aqua");
   // we can also format rgb colors as strings:
   // background("rgb(0,0,0)");
-  background("rgba(179, 234, 200, 1)");
+  background("rgba(188, 236, 207, 1)");
 
   noStroke();
-  fill("#8bd4d2ff");
+  fill("#a3e1dfff");
   rect(0,0,width/2,height/2);// a square in the top left corner
-  
   rect(width/2,height/2,width,height);
   //stroke and fill change the color of drawn shapes
-  stroke("rgba(203, 193, 218, 1)");
-  fill("rgba(246, 198, 219, 1)");
+  stroke("rgba(217, 202, 240, 1)");
+  fill("rgba(209, 134, 167, 1)");
 
   strokeWeight(5);
-  //noStrole(); gets rid of the stroke completely
+  //noStroke(); gets rid of the stroke completely
   //noFill(); //gets rid of the fill completely
 
   //circle takes 3 parameters: x, y and d;
   circle(200,200,50);
   
   // setting a new fill for my rectangle
-  fill("rgba(240, 227, 155, 1)");
+  fill("rgba(242, 230, 166, 1)");
 
   //rect takes 4 parameters:
   //x coord of top left, y coord of top left, width and height
@@ -60,7 +59,7 @@ function draw() {
   vertex(200,150); //bottom-most coordinate
   endShape(CLOSE); //CLOSE parameter closes the polygon
 
-  fill("#f5d2aaff");// colors can also be denoted in hex format
+  fill("#f9ab52ff");// colors can also be denoted in hex format
   circle(width/2,height*0.75,width/2.75);
 
   ellipse(mouseX,mouseY,mouseY,mouseX);
@@ -71,6 +70,8 @@ function draw() {
 
   arc(width/2, height*0.75,100,100,-PI/2, radians(330), CHORD);
 
+
+  
 
 
 }
