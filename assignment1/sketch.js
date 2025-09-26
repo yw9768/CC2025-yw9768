4fc36621918da123d1a6e2d61a147b5fd514d379
yw9768 <yw9768@nyu.edu>
2025-09-26 01:55:49 -0400

    /

--- assignment1/sketch.js
+++ assignment1/sketch.js
@@ -1,7 +1,7 @@
 let stick = 2; //variable to store strokeweight size
 
 function setup() {
-  createCanvas(400,400);// set a 400px by 400ox canvas
+  createCanvas(windowWidth,windowHeight);// set a 400px by 400ox canvas
 }
 
 function draw() {
@@ -13,148 +13,137 @@ function draw() {
   
 
   beginShape(); // draw the first complex polygon
-  vertex(57,183);
-  vertex(105,200);
-  vertex(105,270);
-  vertex(153,289);
-  vertex(60,301);
-  endShape(CLOSE); 
+  vertex(width*0.1425,height*0.4575);
+  vertex(width*0.2625,height*0.5);
+  vertex(width*0.2625,height*0.675);
+  vertex(width*0.3825,height*0.7225);
+  vertex(width*0.15,height*0.75);
+  endShape(CLOSE);  
 
   fill("rgba(199, 247, 239, 1)"); // set the color of the first triangle
-  triangle(60,182,105,177,105,200);
+  triangle(width*0.1425,height*0.4575,width*0.2625,height*0.4425,width*0.2625,height*0.5);
 
   fill("rgba(162, 195, 233, 1)"); // set the color of the second triangle
-  triangle(104,152,105,177,152,170);
+  triangle(width*0.26,height*0.38,width*0.38,height*0.425,width*0.2625,height*0.4425);
 
   fill("rgba(68, 133, 244, 1)"); // set the color of the third triangle
-  triangle(105,151,153,170,152,146);
+  triangle(width*0.26,height*0.38,width*0.38,height*0.365,width*0.38,height*0.425);
 
   fill("rgba(236, 222, 235, 1)"); // set the color of complex polygon
   beginShape(); // draw the second complex polygon
-  vertex(105,177);
-  vertex(153,170);
-  vertex(153,219);
-  vertex(105,200);
+  vertex(width*0.2625,height*0.4425);
+  vertex(width*0.38,height*0.425);
+  vertex(width*0.3825,height*0.5475);
+  vertex(width*0.2625,height*0.5);
   endShape(CLOSE); 
 
   fill("rgba(247, 240, 226, 1)"); // set the color of complex polygon
-  beginShape(); // draw the third complex polygon
-  vertex(105,200);
-  vertex(153,219);
-  vertex(153,264);
-  vertex(105,270);
-  endShape(CLOSE); 
+ beginShape(); // draw the third complex polygon
+ vertex(width*0.2625,height*0.5);
+ vertex(width*0.3825,height*0.5475);
+ vertex(width*0.3825,height*0.66);
+ vertex(width*0.2625,height*0.675);
+ endShape(CLOSE);
 
-  fill("rgba(208, 189, 242, 1)"); // set the color of the fourth triangle
-  triangle(107,270,153,264,153,289);
+ 
+ fill("rgba(208, 189, 242, 1)"); // set the color of the fourth triangle
+ triangle(width*0.265,height*0.675,width*0.3825,height*0.66,width*0.3825,height*0.7225);
 
-  fill("rgba(208, 189, 242, 1)"); // set the color of the fifth triangle
-  triangle(107,270,153,264,153,289);
+ fill("rgba(85, 118, 146, 1)"); // set the color of the fifth triangle
+ triangle(width*0.15,height*0.75,width*0.3825,height*0.7225,width*0.3825,height*0.84);
 
-  fill("rgba(85, 118, 146, 1)"); // set the color of the sixth triangle
-  triangle(60,301,153,289,153,336);
+ fill("rgba(180, 235, 218, 1)"); // set the color of complex polygon
+ beginShape(); // draw the fourth complex polygon
+ vertex(width*0.38,height*0.365);
+ vertex(width*0.5075,height*0.4125);
+ vertex(width*0.505,height*0.53);
+ vertex(width*0.3825,height*0.5475);
+ endShape(CLOSE); 
 
-  fill("rgba(180, 235, 218, 1)"); // set the color of complex polygon
-  beginShape(); // draw the fourth complex polygon
-  vertex(152,146);
-  vertex(203,165);
-  vertex(202,212);
-  vertex(153,219);
-  endShape(CLOSE); 
+ fill("rgba(228, 235, 180, 1)"); // set the color of complex polygon
+ beginShape(); // draw the fifth complex polygon
+ vertex(width*0.3825,height*0.5475);
+ vertex(width*0.505,height*0.53);
+ vertex(width*0.5025,height*0.6475);
+ vertex(width*0.3825,height*0.66);
+ endShape(CLOSE); 
 
-  fill("rgba(228, 235, 180, 1)"); // set the color of complex polygon
-  beginShape(); // draw the fifth complex polygon
-  vertex(153,219);
-  vertex(202,212);
-  vertex(201,259);
-  vertex(153,264);
-  endShape(CLOSE); 
+ fill("rgba(230, 201, 181, 1)"); // set the color of complex polygon
+ beginShape(); // draw the fifth complex polygon
+ vertex(width*0.3825,height*0.66);
+ vertex(width*0.5025,height*0.6475);
+ vertex(width*0.6225,height*0.69);
+ vertex(width*0.6225,height*0.81);
+ vertex(width*0.3825,height*0.84);
+ endShape(CLOSE); 
 
-  fill("rgba(230, 201, 181, 1)"); // set the color of complex polygon
-  beginShape(); // draw the fifth complex polygon
-  vertex(153,264);
-  vertex(201,259);
-  vertex(249,276);
-  vertex(249,324);
-  vertex(153,336);
-  endShape(CLOSE); 
+ fill("rgba(229, 235, 209, 1)"); // set the color of complex polygon
+ beginShape(); // draw the fifth complex polygon
+ vertex(width*0.3775,height*0.2475);
+ vertex(width*0.7475,height*0.38);
+ vertex(width*0.5075,height*0.4125);
+ vertex(width*0.38,height*0.365);
+ endShape(CLOSE); 
 
-  fill("rgba(233, 208, 192, 1)"); // set the color of complex polygon
-  beginShape(); // draw the fifth complex polygon
-  vertex(153,264);
-  vertex(201,259);
-  vertex(249,276);
-  vertex(249,324);
-  vertex(153,336);
-  endShape(CLOSE); 
+ line(width*0.63,height*0.3425,width*0.63,height*0.395);
 
-  fill("rgba(229, 235, 209, 1)"); // set the color of complex polygon
-  beginShape(); // draw the fifth complex polygon
-  vertex(151,99);
-  vertex(299,152);
-  vertex(203,165);
-  vertex(152,146);
-  endShape(CLOSE); 
+ fill("rgba(150, 207, 244, 1)"); // set the color of the seventh triangle
+ triangle(width*0.3775,height*0.2475,width*0.63,height*0.2125,width*0.63,height*0.3425);
 
-  line(252,137,252,158);
+ fill("rgba(251, 202, 203, 1)"); // set the color of complex polygon
+ beginShape(); // draw the sixth complex polygon
+ vertex(width*0.5075,height*0.4125);
+ vertex(width*0.63,height*0.395);
+ vertex(width*0.625,height*0.5125);
+ vertex(width*0.505,height*0.53);
+ endShape(CLOSE); 
 
-  fill("rgba(150, 207, 244, 1)"); // set the color of the seventh triangle
-  triangle(151,99,252,85,252,137);
+ fill("rgba(202, 223, 251, 1)"); // set the color of complex polygon
+ beginShape(); // draw the seventh complex polygon
+ vertex(width*0.505,height*0.53);
+ vertex(width*0.625,height*0.5125);
+ vertex(width*0.6225,height*0.69);
+ vertex(width*0.5025,height*0.6475);
+ endShape(CLOSE); 
 
-  fill("rgba(251, 202, 203, 1)"); // set the color of complex polygon
-  beginShape(); // draw the sixth complex polygon
-  vertex(203,165);
-  vertex(252,158);
-  vertex(250,205);
-  vertex(202,212);
-  endShape(CLOSE); 
+ fill("rgba(144, 236, 146, 1)"); // set the color of complex polygon
+ beginShape(); // draw the eighth complex polygon
+ vertex(width*0.63,height*0.395);
+ vertex(width*0.7475,height*0.38);
+ vertex(width*0.7425,height*0.555);
+ vertex(width*0.625,height*0.5125);
+ endShape(CLOSE); 
 
-  fill("rgba(202, 223, 251, 1)"); // set the color of complex polygon
-  beginShape(); // draw the seventh complex polygon
-  vertex(202,212);
-  vertex(250,205);
-  vertex(249,276);
-  vertex(201,259);
-  endShape(CLOSE); 
+ fill("rgba(236, 175, 144, 1)"); // set the color of complex polygon
+ beginShape(); // draw the nighth complex polygon
+ vertex(width*0.625,height*0.5125);
+ vertex(width*0.7425,height*0.555);
+ vertex(width*0.74,height*0.615);
+ vertex(width*0.6225,height*0.69);
+ endShape(CLOSE); 
 
-  fill("rgba(144, 236, 146, 1)"); // set the color of complex polygon
-  beginShape(); // draw the eighth complex polygon
-  vertex(252,158);
-  vertex(299,152);
-  vertex(297,222);
-  vertex(250,205);
-  endShape(CLOSE); 
+ fill("rgba(202, 207, 232, 1)"); // set the color of complex polygon
+ beginShape(); // draw the tenth complex polygon
+ vertex(width*0.6225,height*0.63);
+ vertex(width*0.74,height*0.615);
+ vertex(width*0.74,height*0.6725);
+ vertex(width*0.6225,height*0.69);
+ endShape(CLOSE);  
 
-  fill("rgba(236, 175, 144, 1)"); // set the color of complex polygon
-  beginShape(); // draw the nighth complex polygon
-  vertex(250,205);
-  vertex(297,222);
-  vertex(296,246);
-  vertex(249,252);
-  endShape(CLOSE); 
+ line(width*0.6225,height*0.63,width*0.74,height*0.6725);
 
-  fill("rgba(202, 207, 232, 1)"); // set the color of complex polygon
-  beginShape(); // draw the tenth complex polygon
-  vertex(249,252);
-  vertex(296,246);
-  vertex(296,269);
-  vertex(249,276);
-  endShape(CLOSE); 
+ fill("rgba(202, 207, 232, 1)"); // set the color of complex polygon
+ beginShape(); // draw the eleventh complex polygon
+ vertex(width*0.63,height*0.2125);
+ vertex(width*0.875,height*0.3075);
+ vertex(width*0.8625,height*0.6);
+ vertex(width*0.7425,height*0.555);
+ vertex(width*0.7475,height*0.38);
+ vertex(width*0.63,height*0.3425);
+ endShape(CLOSE); 
 
-  line(249,252,296,269);
-
-  fill("rgba(202, 207, 232, 1)"); // set the color of complex polygon
-  beginShape(); // draw the eleventh complex polygon
-  vertex(252,85);
-  vertex(350,123);
-  vertex(345,240);
-  vertex(297,222);
-  vertex(299,152);
-  vertex(252,137);
-  endShape(CLOSE); 
-
-  fill("rgba(85, 118, 146, 1)"); // set the color of the seventh triangle
-  triangle(297,222,345,240,296,246);
+ fill("rgba(85, 118, 146, 1)"); // set the color of the seventh triangle
+ triangle(width*0.7425,height*0.555,width*0.8625,height*0.6,width*0.74,height*0.615);
 
 
 
