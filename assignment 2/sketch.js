3a1c567abc900baac618522753e7f4f420e167f6
yw9768 <yw9768@nyu.edu>
2025-09-30 03:55:25 -0400

    assignment2

--- assignment 2/sketch.js
+++ assignment 2/sketch.js
@@ -1,6 +1,10 @@
 let cellW = 75, cellH = 45; // set the width and height of bricks
 let margin = 20;            // set the blank space around the bricks
 
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
@@ -8,7 +12,7 @@ function setup() {
 }
 
 function draw() {
-  background("rgba(218, 222, 221, 1)"); // gray bg
+  background("rgba(245, 247, 223, 1)"); // gray bg
   translate(margin, margin);
 
   const usableW = width  - margin * 2;
@@ -25,14 +29,14 @@ function draw() {
      t = 0;                      // 防止除 0
       }
 
-
+     
 
       // 颜色：mouseX 控制（左→右渐变）
       // 用 lerp 在两种颜色之间插值：从(93,107,158)到(220,120,80)
       const u = constrain(mouseX / width, 0, 1);//左边最小值是0，右边最大值是1
-      const r = lerp(93,  220, u);
-      const g = lerp(107, 120, u);
-      const b = lerp(158,  80, u);//分别代表rgb颜色三个值的变化
+      const r = lerp(64, 120, u);
+      const g = lerp(180, 200, u);
+      const b = lerp(170, 255, u);//分别代表rgb颜色三个值的变化
 
       // 旋转角：越往下越大，并受 mouseY 调整
       // mouseY 在顶部=0旋转，底部≈PI/3；整体再乘以 t（行数）
@@ -43,19 +47,25 @@ function draw() {
       const bw = cellW * 0.9;
       const bh = cellH * 0.7;
 
+      
       push();
       // 把原先“左上角”的定位改为“格子中心”，便于旋转
       translate(x + cellW / 2, y + cellH / 2);//因为现在用的是corner， 旋转对准的是格子中心
       //但是xy在左上角，所以要移到砖块中间
       rotate(ang);
+      
+     if (mouseIsPressed) {
+       ellipse(0, 0, bw, bh);     // 按下→椭圆并有抖动
+      } else {
+      rect(0, 0, bw, bh, 5);     // 未按→圆角矩形
+       }
+      pop();
 
-      fill(r, g, b);
-      rect(0, 0, bw, bh, 5);
+      
+      colorMode(RGB);
+      fill(r,g,b);
       pop();
     }
   }
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
