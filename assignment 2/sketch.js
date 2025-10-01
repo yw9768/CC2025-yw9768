f40cd2a0523e4b6692e2fbb3f420f05f8becf36a
yw9768 <yw9768@nyu.edu>
2025-10-01 02:00:30 -0400

    assignment 2

--- assignment 2/sketch.js
+++ assignment 2/sketch.js
@@ -3,68 +3,82 @@ let margin = 20;            // set the blank space around the bricks
 
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth, windowHeight);//set the adaptive window size
 }
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);//create a canvas as large as the window
   noStroke();
-  rectMode(CENTER); // 以中心画矩形，便于绕中心旋转
+  rectMode(CENTER); // draw a rectangle with the center to 
+  // facilitate the subsequent rotation of the bricks around the center
 }
 
 function draw() {
-  background("rgba(245, 247, 223, 1)"); // gray bg
-  translate(margin, margin);
+  background("rgba(245, 247, 223, 1)"); // yellow bg
+  translate(margin, margin);// i move the margin of the coordinate origin 
+  // as a whole to the lower right 
+  // then all subsequent drawing should be carried out 
+  // within the inner margin box
 
   const usableW = width  - margin * 2;
-  const usableH = height - margin * 2;
+  const usableH = height - margin * 2;//i calculated the drawable area
+  //  removed the margins on the left, right and top and bottom,
+  // and obtained the actual internal width and height that can be used to arrange the grid
 
-  for (let x = 0; x <= usableW - cellW; x += cellW) {       // 列
-    for (let y = 0; y <= usableH - cellH; y += cellH) {     // 行
-      // 0（上）→ 1（下），用于随Y轴增强旋转/尺寸等
+
+  for (let x = 0; x <= usableW - cellW; x += cellW) {       // create a variable called x
+  // from left to right, move one cellW each time. continue the loop as long as one brick can still be placed intact
+    for (let y = 0; y <= usableH - cellH; y += cellH) {     // create a variable called y
+    // from top to bottom, move one cellH each time. continue the loop as long as one brick can still be placed intact
+
      
-    let t; //行数
+  
+    let t; //set a ratio t
     if (usableH - cellH > 0) {
-     t = y / (usableH - cellH);  // 顶部 ~0，底部 ~1
+     t = y / (usableH - cellH);  // the distance already traveled/the total distance
       } else {
-     t = 0;                      // 防止除 0
+     t = 0;  // the value of t is a variable that becomes increasingly complex along the Y-axis, 
+             // the rotation angle will increase synchronously as t increases
       }
 
      
 
-      // 颜色：mouseX 控制（左→右渐变）
-      // 用 lerp 在两种颜色之间插值：从(93,107,158)到(220,120,80)
-      const u = constrain(mouseX / width, 0, 1);//左边最小值是0，右边最大值是1
+      // set the color gradient from left to right controlled by mouseX
+      // interpolate between two colors using lerp: from (64,180,170) to (120,200,255)
+      const u = constrain(mouseX / width, 0, 1);//set the range of mouseX, 
+                  // with the minimum value on the left being 0 
+                  // and the maximum value on the right being 1
       const r = lerp(64, 120, u);
       const g = lerp(180, 200, u);
-      const b = lerp(170, 255, u);//分别代表rgb颜色三个值的变化
+      const b = lerp(170, 255, u);
 
-      // 旋转角：越往下越大，并受 mouseY 调整
-      // mouseY 在顶部=0旋转，底部≈PI/3；整体再乘以 t（行数）
-      const maxAng = map(mouseY, 0, height, 0, PI / 3);//到最下面旋转60度
+      // start designing the rotation angle
+      //mouseY rotates 0 at the top and PI/3 (60 degree) at the bottom
+      const maxAng = map(mouseY, 0, height, 0, PI / 3);
       const ang = t * maxAng;
 
-      // 砖实际尺寸（留缝）
+      // make the actual brick size slightly smaller than
+      // the original size (leave a gap for a cleaner visual effect)
       const bw = cellW * 0.9;
       const bh = cellH * 0.7;
 
       
       push();
-      // 把原先“左上角”的定位改为“格子中心”，便于旋转
-      translate(x + cellW / 2, y + cellH / 2);//因为现在用的是corner， 旋转对准的是格子中心
-      //但是xy在左上角，所以要移到砖块中间
-      rotate(ang);
+      translate(x + cellW / 2, y + cellH / 2);
+      //because the original coordinates x and y were in the upper left corner, 
+       // they have now been moved to the center of the current brick
+      rotate(ang); //rotate an ang arc around the center of the brick
       
+       
+      fill(r,g,b);// set the color of shapes
+      //switch shapes based on whether the mouse is pressed or not
      if (mouseIsPressed) {
-       ellipse(0, 0, bw, bh);     // 按下→椭圆并有抖动
+       ellipse(0, 0, bw, bh);     // press the mouse to turn it into an ellipse
       } else {
-      rect(0, 0, bw, bh, 5);     // 未按→圆角矩形
+      rect(0, 0, bw, bh, 5);     // release--go back to rect
        }
       pop();
 
-      
-      colorMode(RGB);
-      fill(r,g,b);
-      pop();
+     
     }
   }
 }
