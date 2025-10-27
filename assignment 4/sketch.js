8f68e20c2f69ef07d97e4227dabd94991bb20318
yw9768 <yw9768@nyu.edu>
2025-10-27 03:07:02 -0400

    /

--- assignment 4/sketch.js
+++ assignment 4/sketch.js
@@ -12,7 +12,7 @@ function setup() {
   noStroke();
 
   //// set the size of the pad will draw later
-  padSize = width * 0.2;
+  padSize = height*0.5;
   padX = width / 2;
   padY = height / 2;
   padR = padSize / 2;
